ab02b2c10f0fe6824cf38cbfb7e6d72d8177cd57
Erhan Ertem <eertem@gmail.com>
2024-04-25 15:16:20 +0300

    Provide the missing variable declaration

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ const PORT = 3000;
 server.listen(PORT);
 console.log(`Listening on port ${PORT}...`);
 
+let readyPlayerCount = 0;
+
 io.on('connection', (socket) => {
 	console.log('a user connected @socketID: ', socket.id);
 
