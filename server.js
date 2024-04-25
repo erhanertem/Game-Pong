2bd34d24252d58d136df0814620bbcc848e5272a
Erhan Ertem <eertem@gmail.com>
2024-04-25 13:29:42 +0300

    Adopt 2 ways of implementing socket.io over node.js server

---
+++ server.js
@@ -0,0 +1,21 @@
+// // Alternative #1
+// const { createServer } = require('http');
+// const { Server } = require('socket.io');
+// const PORT = 3000;
+// const httpServer = createServer();
+// const io = new Server(httpServer, {
+// 	// options
+// });
+// io.listen(PORT);
+// console.log(`Listening on port ${PORT}...`);
+
+// Alternative #2
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+const PORT = 3000;
+server.listen(PORT);
+console.log(`Listening on port ${PORT}...`);
+
+io.on('connection', (socket) => {
+	console.log('a user connected');
+});
