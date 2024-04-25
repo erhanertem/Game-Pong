454e330c90b25e0e6ef88658a02a211cb385227a
Erhan Ertem <eertem@gmail.com>
2024-04-25 19:30:34 +0300

    Export express api to nodejs server

--- api.js
+++ api.js
@@ -12,3 +12,6 @@ api.use(express.static(path.join(__dirname, 'public')));
 api.use('/', express.static('index.html'));
 
 api.listen(PORT, () => console.log(`Listening xpress on ${PORT}...`));
+
+// NOTE Export express server to pass onto nodejs http server to bridge with socket.io
+module.exports = { api };
