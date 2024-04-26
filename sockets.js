585035e4f07d6489ce5ff2185643934f9dfd094e
Erhan Ertem <eertem@gmail.com>
2024-04-26 14:08:58 +0300

    Create room @ pong namespace and explicitly trash socket upon client disconnect

--- sockets.js
+++ sockets.js
@@ -2,7 +2,7 @@ let readyPlayerCount = 0;
 
 function listen(io) {
 	// Hosting multiple io sockets within the same pipeline
-	const tetrisNamespace = io.of('/tetris');
+	// const tetrisNamespace = io.of('/tetris');
 	const pongNamespace = io.of('/pong');
 
 	// Seperation of concern for different namespaces
@@ -10,8 +10,18 @@ function listen(io) {
 		console.log('a user connected @socketID: ', socket.id);
 
 		socket.on('ready', () => {
-			console.log('Player ready', socket.id);
+			// PLAYER1 (READYPLAYER COUNT 0) AND PLAYER 2 (READYPLAYER COUNT 1)
+			let room = 'room' + Math.floor(readyPlayerCount / 2);
+			socket.join(room);
 
+			console.log(
+				'Player ready',
+				socket.id,
+				'Belongs to namespace @ ',
+				pongNamespace.name,
+				' room @ ',
+				room
+			);
 			readyPlayerCount++;
 
 			// Broadcast to all clients - sending the second opponent as refree id
@@ -34,6 +44,8 @@ function listen(io) {
 		// Listen for Client Socket Disconnects
 		socket.on('disconnect', (reason) => {
 			console.log(`Client ${socket.id} disconnected: ${reason}`);
+			// Discharge the socket upon leave
+			socket.leave(room);
 		});
 	});
 }
